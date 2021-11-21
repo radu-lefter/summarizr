1b61d80f2baa8990d4bac1e08c781ec033077504
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2021-11-21 22:36:44 +0000

    Database Connection Setup complete

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-// Route files
-const summaries = require('./routes/summaries');
+
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const summaries = require('./routes/summaries');
 
 const app = express();
 
@@ -15,13 +22,22 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
   }
   
-  // Mount routers
-  app.use('/api/v1/summaries', summaries);
+// Mount routers
+app.use('/api/v1/summaries', summaries);
   
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+const server = app.listen(
+    PORT,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+  );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+  });
