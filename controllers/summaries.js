14695b0f71dc146862c245b6395cf0cf7ff49ca7
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2021-11-23 00:36:31 +0000

    Error handling and slugify  implemented

--- controllers/summaries.js
+++ controllers/summaries.js
@@ -1,86 +1,84 @@
 const Summary = require('../models/Summary');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc      Get all summaries
 // @route     GET /api/v1/summaries
 // @access    Public
-exports.getSummaries = async (req, res, next) => {
-  try {
+exports.getSummaries = asyncHandler(async (req, res, next) => {
+ 
     const summaries = await Summary.find();
 
     res
       .status(200)
       .json({ success: true, count: summaries.length, data: summaries });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  
+});
   
   // @desc      Get single summary
   // @route     GET /api/v1/summaries/:id
   // @access    Public
-  exports.getSummary = async (req, res, next) => {
-    try {
+  exports.getSummary = asyncHandler(async (req, res, next) => {
+
       const summary = await Summary.findById(req.params.id);
   
       if (!summary) {
-        return res.status(400).json({ success: false });
+        return next(
+          new ErrorResponse(`Summary not found with id of ${req.params.id}`, 404)
+        );
       }
   
       res.status(200).json({ success: true, data: summary });
-    } catch (err) {
-      res.status(400).json({ success: false });
-    }
-  };
+
+  });
   
   // @desc      Create new summary
   // @route     POST /api/v1/summaries
   // @access    Private
-  exports.createSummary = async (req, res, next) => {
-    try {
+  exports.createSummary = asyncHandler(async (req, res, next) => {
+
       const summary = await Summary.create(req.body);
   
       res.status(201).json({
         success: true,
         data: summary
       });
-    } catch (err) {
-      res.status(400).json({ success: false });
-    }
-  };
+ 
+  });
   
   // @desc      Update summary
   // @route     PUT /api/v1/summaries/:id
   // @access    Private
-  exports.updateSummary = async (req, res, next) => {
-    try {
+  exports.updateSummary = asyncHandler(async (req, res, next) => {
+
       const summary = await Summary.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true
       });
   
       if (!summary) {
-        return res.status(400).json({ success: false });
-      }
+        return next(
+          new ErrorResponse(`Summary not found with id of ${req.params.id}`, 404)
+        );
+      };
   
       res.status(200).json({ success: true, data: summary });
-    } catch (err) {
-      res.status(400).json({ success: false });
-    }
-  };
+
+  });
   
   // @desc      Delete summary
   // @route     DELETE /api/v1/summaries/:id
   // @access    Private
-  exports.deleteSummary = async (req, res, next) => {
-    try {
+  exports.deleteSummary = asyncHandler(async (req, res, next) => {
+
       const summary = await Summary.findByIdAndDelete(req.params.id);
   
       if (!summary) {
-        return res.status(400).json({ success: false });
+        return next(
+          new ErrorResponse(`Summary not found with id of ${req.params.id}`, 404)
+        );
       }
   
       res.status(200).json({ success: true, data: {} });
-    } catch (err) {
-      res.status(400).json({ success: false });
-    }
-  };
+
+  });
--- models/Summary.js
+++ models/Summary.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require('slugify');
 
 const SummarySchema = new mongoose.Schema({
   name: {
@@ -53,6 +54,12 @@ const SummarySchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create summary slug from the name
+SummarySchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Summary", SummarySchema);
