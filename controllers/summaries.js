1dd7295e654a57d968695e1d20a81a53a8785c15
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2021-11-23 16:53:49 +0000

    Implemented sorting and pagination

--- controllers/summaries.js
+++ controllers/summaries.js
@@ -6,20 +6,76 @@ const asyncHandler = require('../middleware/async');
 // @route     GET /api/v1/summaries
 // @access    Public
 exports.getSummaries = asyncHandler(async (req, res, next) => {
- 
+  
   let query;
+  
+  // Copy req.query
+  const reqQuery = { ...req.query };
 
-  let queryStr = JSON.stringify(req.query);
+  // Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
+  // Finding resource
   query = Summary.find(JSON.parse(queryStr));
 
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Summary.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  // Executing query
   const summaries = await query;
 
-    res
-      .status(200)
-      .json({ success: true, count: summaries.length, data: summaries });
+    // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: summaries.length,
+    pagination,
+    data: summaries
+  });
   
 });
   
