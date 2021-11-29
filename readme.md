f84b03f838bf57002d917f901c5becbefe6f1a63
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2021-11-29 15:00:50 +0000

    Implemented sanitization and security

---
+++ readme.md
@@ -0,0 +1,45 @@
+# Summarizr API
+
+> Backend API for Summarizr application, which is a summary collection website
+
+## Usage
+
+Rename "config/config.env.env" to "config/config.env" and update the values/settings to your own
+
+## Install Dependencies
+
+```
+npm install
+```
+
+## Run App
+
+```
+# Run in dev mode
+npm run dev
+
+# Run in prod mode
+npm start
+```
+
+## Database Seeder
+
+To seed the database with users, summaries, translations and reviews with data from the "\_data" folder, run
+
+```
+# Destroy all data
+node seeder -d
+
+# Import all data
+node seeder -i
+```
+
+## Demo
+
+The API is live at [devcamper.io]()
+
+Extensive documentation with examples [here](https://documenter.getpostman.com/view/14302851/UVJcmcEb)
+
+- Version: 1.0.0
+- License: MIT
+- Author: Radu Lefter
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const connectDB = require('./config/db');
 const path = require('path');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 
 
@@ -38,6 +44,29 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
